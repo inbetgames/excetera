67c12c0a221d5377c75e14e0af6e9444c7dfbb25
d0rc <7993863@gmail.com>
2013-11-29 16:30:23 +0400

    more functions

--- lib/diamorfosi.ex
+++ lib/diamorfosi.ex
@@ -2,7 +2,7 @@ defmodule Diamorfosi do
   use Application.Behaviour
 
   @etcd "http://my.host.name:4001/v1/keys"
-
+  @timeout 5000
   # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -11,23 +11,27 @@ defmodule Diamorfosi do
 
   defp body_encode(list) do
   	Keyword.keys(list) 
-  		|> Enum.map(fn key ->
+  		|> Stream.map(fn key ->
   			"#{key}=#{list[key]}"
   		end)
   		|> Enum.join("&")
   end
 
-  def get(path) do
-  	case HTTPotion.get "#{@etcd}#{path}" do
+  def get(path, options // []) do
+  	timeout = Keyword.get options, :timeout, @timeout
+  	options = Keyword.delete options, :timeout
+  	case HTTPotion.get "#{@etcd}#{path}", [], [timeout: timeout] do
   		HTTPotion.Response[status_code: 200, body: body] -> body |> JSEX.decode!
   		_ -> false
   	end
   end
   def set(path, value, options // []) do
-  	IO.puts "body: #{body_encode([value: value] ++ options)}"
-  	case HTTPotion.request :put, "#{@etcd}#{path}", body_encode([value: value] ++ options), [{:'Content-Type', "application/x-www-form-urlencoded"}], [timeout: 1000] do
+  	timeout = Keyword.get options, :timeout, @timeout
+  	options = Keyword.delete options, :timeout
+  	case HTTPotion.request :put, "#{@etcd}#{path}", body_encode([value: value] ++ options), [{:'Content-Type', "application/x-www-form-urlencoded"}], [timeout: timeout] do
   		HTTPotion.Response[status_code: 200, body: body] -> body |> JSEX.decode!
   		_ -> false
   	end
   end
+  def wait(path, options // []), do: get("#{path}?wait=true", options)
 end
