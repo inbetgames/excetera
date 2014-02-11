95e470e3fa1a461e919497861df10d71d4a19d25
d0rc <7993863@gmail.com>
2014-02-11 06:35:47 +0400

    added tests/examples

--- test/diamorfosi_test.exs
+++ test/diamorfosi_test.exs
@@ -1,7 +1,19 @@
 defmodule DiamorfosiTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "setting values" do
+    Diamorfosi.set "/test", "value"
+    assert Diamorfosi.get("/test") == "value"
+  end
+
+  test "setting complex values" do
+  	Diamorfosi.set "/test", [{"some", "value"}]
+  	assert Diamorfosi.get("/test") == [{"some", "value"}]
+  end
+
+  test "setting with TTL" do
+  	Diamorfosi.set "/test", "valuex", [ttl: 1]
+  	:timer.sleep 1500
+  	assert Diamorfosi.get("/test") == false
   end
 end
