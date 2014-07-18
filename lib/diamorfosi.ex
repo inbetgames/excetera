77f57ee91498cc021c6672d8a0a2e485c9e0a333
Alexei Sholik <alcosholik@gmail.com>
2014-07-18 23:48:18 +0300

    Tabs to spaces

--- lib/diamorfosi.ex
+++ lib/diamorfosi.ex
@@ -33,11 +33,11 @@ defmodule Diamorfosi do
   end
 
   def get_with_details(path, options \\ []) do
-  	timeout = Keyword.get options, :timeout, @timeout
-  	case HTTPoison.get "#{etcd_url}#{path}", [], [timeout: timeout] do
-  		%HTTPoison.Response{status_code: 200, body: body} -> body |> Jazz.decode!
-  		_ -> false
-  	end
+    timeout = Keyword.get options, :timeout, @timeout
+    case HTTPoison.get "#{etcd_url}#{path}", [], [timeout: timeout] do
+      %HTTPoison.Response{status_code: 200, body: body} -> body |> Jazz.decode!
+      _ -> false
+    end
   end
 
   def set_term(path, value) do
@@ -52,29 +52,29 @@ defmodule Diamorfosi do
 
   def set(path, value), do: set(path, value, [])
   def set(path, value, options) when is_binary(value) do
-  	timeout = Keyword.get options, :timeout, @timeout
-  	options = Keyword.delete options, :timeout
-  	case HTTPoison.request :put, "#{etcd_url}#{path}", body_encode([value: value] ++ options), [{"Content-Type", "application/x-www-form-urlencoded"}], [timeout: timeout] do
-  		%HTTPoison.Response{status_code: code, body: body} when code in [200, 201] -> body |> Jazz.decode!
-  		%HTTPoison.Response{status_code: 307} -> set path, value, options
+    timeout = Keyword.get options, :timeout, @timeout
+    options = Keyword.delete options, :timeout
+    case HTTPoison.request :put, "#{etcd_url}#{path}", body_encode([value: value] ++ options), [{"Content-Type", "application/x-www-form-urlencoded"}], [timeout: timeout] do
+      %HTTPoison.Response{status_code: code, body: body} when code in [200, 201] -> body |> Jazz.decode!
+      %HTTPoison.Response{status_code: 307} -> set path, value, options
       _ -> false
-  	end
+    end
   end
   def set(path, value, options) do
     set(path, Jazz.encode!(value), options)
   end
 
   def wait(path, options \\ []) do
-  	case options[:waitIndex] do
-  		nil ->
-		  	get("#{path}", options)
-		  	|> (fn reply ->
-		  		wait path, Keyword.update(options, :waitIndex, (reply["modifiedIndex"] + 1), &(&1))
-		  	end).()
-		 value when is_integer(value) ->
-		 	options = Keyword.delete options, :waitIndex
-		 	get("#{path}?wait=true&waitIndex=#{value}", options)
-	end
+    case options[:waitIndex] do
+      nil ->
+        get("#{path}", options)
+        |> (fn reply ->
+          wait path, Keyword.update(options, :waitIndex, (reply["modifiedIndex"] + 1), &(&1))
+        end).()
+     value when is_integer(value) ->
+      options = Keyword.delete options, :waitIndex
+      get("#{path}?wait=true&waitIndex=#{value}", options)
+  end
   end
 
 
--- lib/diamorfosi/registry.ex
+++ lib/diamorfosi/registry.ex
@@ -1,35 +1,35 @@
 defmodule Diamorfosi.Registry do
-	def atom_open(atomname, _default_value \\ nil) do
-		worker_name = "Diamorfosi.#{atomname}" |> :erlang.binary_to_atom(:utf8)
-		:supervisor.start_child(
-			Diamorfosi.Supervisor,
-			Supervisor.Behaviour.worker(Diamorfosi.Registry.Worker, [atomname], [worker_name])
-		)
-	end
+  def atom_open(atomname, _default_value \\ nil) do
+    worker_name = "Diamorfosi.#{atomname}" |> :erlang.binary_to_atom(:utf8)
+    :supervisor.start_child(
+      Diamorfosi.Supervisor,
+      Supervisor.Behaviour.worker(Diamorfosi.Registry.Worker, [atomname], [worker_name])
+    )
+  end
 
-	def atom_close(_atomname, _delete \\ true) do
+  def atom_close(_atomname, _delete \\ true) do
 
-	end
+  end
 
-	def atom_get(atomname) do
-		:ets.lookup(:diamorfosi_registry, atomname)
-	end
+  def atom_get(atomname) do
+    :ets.lookup(:diamorfosi_registry, atomname)
+  end
 
-	def atom_set(atomname, value) do
-		Diamorfosi.set "/refs/#{atomname}", Diamorfosi.Serialize.serialize(value)
-	end
+  def atom_set(atomname, value) do
+    Diamorfosi.set "/refs/#{atomname}", Diamorfosi.Serialize.serialize(value)
+  end
 end
 
 defmodule Diamorfosi.Registry.Worker do
   use GenServer
-	require Lager
+  require Lager
 
-	def init(atomname) do
-		{:ok, %{atom: atomname}, 0}
-	end
+  def init(atomname) do
+    {:ok, %{atom: atomname}, 0}
+  end
 
-	def handle_info(:timeout, %{atom: atomname}=state) do
-		Lager.info "Fetching new value for atom #{inspect atomname}"
-		{:noreply, state, 1000}
-	end
+  def handle_info(:timeout, %{atom: atomname}=state) do
+    Lager.info "Fetching new value for atom #{inspect atomname}"
+    {:noreply, state, 1000}
+  end
 end
