1b0fa7964c163eb0f14fb6f8c704897df7ece8be
Alexei Sholik <alcosholik@gmail.com>
2014-07-21 21:21:20 +0300

    Refine API.get and API.put

--- lib/diamorfosi/api.ex
+++ lib/diamorfosi/api.ex
@@ -5,15 +5,11 @@ defmodule Diamorfosi.API do
 
   alias HTTPoison.Response, as: HttpResp
 
-  def get("/"<>_=keypath, options) do
+  def get("/"<>_=keypath, api_options, options \\ []) do
     timeout = Keyword.get(options, :timeout, @default_timeout)
-    {recursive, options} = Keyword.pop(options, :recursive, nil)
-    {sorted, options} = Keyword.pop(options, :sort, nil)
-
-    params = [{"recursive", recursive}, {"sorted", sorted}]
 
     headers = []
-    url = "#{etcd_url}#{keypath}#{params_to_query_string(params)}"
+    url = "#{etcd_url}#{keypath}#{params_to_query_string(api_options)}"
 
     #    case options[:waitIndex] do
     #      nil ->
@@ -28,37 +24,36 @@ defmodule Diamorfosi.API do
 
     case HTTPoison.get(url, headers, [timeout: timeout]) do
       %HttpResp{status_code: 200, body: body} ->
-        {:ok, Jazz.decode!(body)}
+        {:ok, decode_body(body, options)}
 
       %HttpResp{status_code: status, body: body} ->
-        {:error, status, Jazz.decode!(body)}
+        {:error, status, decode_body(body, options)}
     end
   end
 
-  def put("/"<>_=keypath, value, options) do
-    {timeout, options} = Keyword.pop(options, :timeout, @default_timeout)
-    {return_body, options} = Keyword.pop(options, :return_body, false)
-    {dir, options} = Keyword.pop(options, :dir, nil)
+  def put("/"<>_=keypath, value, api_options, options \\ []) do
+    timeout = Keyword.get(options, :timeout, @default_timeout)
 
-    params = [{"dir", dir}]
+    {return_body, options} = Keyword.pop(options, :return_body, false)
 
     headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
-    url = "#{etcd_url}#{keypath}#{params_to_query_string(params)}"
-    unless dir, do: options = [value: value] ++ options
-    body = encode_body(options)
+    url = "#{etcd_url}#{keypath}#{params_to_query_string(api_options)}"
+    body = if api_options[:dir] do
+      ""
+    else
+      "value=#{value}"
+    end
 
     case HTTPoison.request(:put, url, body, headers, [timeout: timeout]) do
       %HttpResp{status_code: code, body: body} when code in [200, 201] ->
-        if return_body do
-          {:ok, body |> Jazz.decode!}
-        else
-          :ok
-        end
-      %HttpResp{status_code: 307, headers: headers} ->
-        IO.inspect headers
-        put(keypath, value, options)
+        {:ok, decode_body(body, options)}
+
+      #%HttpResp{status_code: 307, headers: headers} ->
+        #IO.inspect headers
+        #put(keypath, value, options)
+
       %HttpResp{status_code: status, body: body} ->
-        {:error, status, Jazz.decode!(body)}
+        {:error, status, decode_body(body, options)}
     end
   end
 
@@ -103,6 +98,12 @@ defmodule Diamorfosi.API do
       |> Enum.join("&")
     if params != "", do: params = "?" <> params
     params
+  end
+
+  defp decode_body(body, options) do
+    if Keyword.get(options, :decode_body, true) do
+      Jazz.decode!(body)
+    end
   end
 
   defp encode_body(list) do
--- test/diamorfosi/api_test.exs
+++ test/diamorfosi/api_test.exs
@@ -2,10 +2,67 @@ defmodule DiamorfosiTest.ApiTest do
   use ExUnit.Case
 
   import DiamorfosiTest.Helpers
+  alias Diamorfosi.API
 
   setup_all do
-    on_exit fn -> cleanup("/api_test") end
     cleanup("/api_test")
   end
 
+  setup do
+    on_exit fn -> cleanup("/api_test") end
+  end
+
+  test "get bad option" do
+    API.put("/api_test/a", "hello", [])
+    assert {:ok, %{"action" => "get"}} = API.get("/api_test/a", [donut: false])
+
+    #assert_raise Diamorfosi.OptionError, "Bad option: {:donut, false}", fn ->
+    #  API.get("/api_test", [], donut: false)
+    #end
+  end
+
+  test "get file and dir" do
+    assert {:error, 404, %{"message" => "Key not found"}}
+           = API.get("/api_test", [])
+    assert {:error, 404, nil} = API.get("/api_test", [], decode_body: false)
+
+    API.put("/api_test/a", "hello", [])
+
+    assert {:ok, %{"action" => "get", "node" => %{}}} = API.get("/api_test/a", [])
+    assert {:ok, nil} = API.get("/api_test/a", [], decode_body: false)
+    assert {:ok, %{"action" => "get", "node" => %{"dir" => true, "nodes" => [%{"value" => "hello"}]}}}
+           = API.get("/api_test", [])
+  end
+
+  test "wait" do
+  end
+
+  test "put file and dir" do
+    assert {:ok, %{"action" => "set", "node" => %{"value" => "hello"}}}
+           = API.put("/api_test/a", "hello", [])
+    assert {:ok, %{"action" => "set", "node" => %{"dir" => true}, "prevNode" => %{"value" => "hello"}}}
+           = API.put("/api_test/a", nil, dir: true)
+
+    assert {:ok, nil} = API.put("/api_test/b", "hello", [], decode_body: false)
+    assert {:ok, nil} = API.put("/api_test/a/b/c", "hello", [], decode_body: false)
+    assert {:ok, %{"node" => %{"value" => "hello"}}} = API.get("/api_test/a/b/c", [])
+  end
+
+  test "put prevIndex" do
+  end
+
+  test "compare and swap" do
+  end
+
+  test "compare and delete" do
+  end
+
+  test "post dir" do
+  end
+
+  test "delete file" do
+  end
+
+  test "delete dir" do
+  end
 end
